59bd467f7bf2d9ce85b65b980a8b6df1a0334b52
Freerk de Leeuw <Github@deleeuw.frl>
2017-01-19 20:47:43 +0100

    Added common prism project

---
+++ PL.Common.Prism/CommandHelper.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Linq;
+using System.Reflection;
+
+namespace PL.Common.Prism
+{
+    /// <summary>Helper for commands.
+    /// </summary>
+    public static class CommandHelper
+    {
+        /// <summary>Invokes the ChangeCanExecute of all commands of the target VM.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        public static void InvokeChangeCanExecute(this BindableBase target)
+        {
+            if (target != null)
+            {
+                var commands = target.GetType()
+                    // Only the public ICommand properties are of interest. The private's
+                    // can never be visible in XAML. Moreover, the private's are probably
+                    // the backing fields only.
+                    .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    // Get only the properties of type ICommand.
+                    .Where(pi => pi.PropertyType == typeof(System.Windows.Input.ICommand))
+                    // Determine the ChangeCanExecute method on that command. It depends
+                    // on the implementation of the interface whether this method is available.
+                    // RelayCommand and RelayCommand<T> both have it.
+                    .Select(pi => pi.GetValue(target, null));
+                foreach (var commandInterface in commands)
+                {
+                    var cmdObj = commandInterface as DelegateCommandBase;
+                    if (cmdObj != null)
+                    {
+                        cmdObj.RaiseCanExecuteChanged();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PL.Common.Prism/ViewModelBase.cs
@@ -0,0 +1,39 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace PL.Common.Prism
+{
+    /// <summary>Base class for ViewModels, introducing the NotifyPropertyChanged method which automatically resolves the property name.
+    /// Also, the commands of the view model will be updated when a single property changes.
+    /// </summary>
+    public abstract class ViewModelBase : BindableBase
+    {
+        #region INotifyPropertyChanged
+
+        /// <summary>Notifies the property changed.
+        /// </summary>
+        /// <param name="propertyName">Name of a property.</param>
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            base.OnPropertyChanged(propertyName);
+            this.InvokeChangeCanExecute();
+            UpdateCommandCanExecute();
+        }
+
+        #endregion INotifyPropertyChanged
+
+        private void UpdateCommandCanExecute()
+        {
+            foreach (var command in this.GetType()
+                .GetProperties()
+                .Where(pi => pi.PropertyType == typeof(DelegateCommand))
+                .Select(pi => pi.GetValue(this))
+                .Cast<DelegateCommand>())
+            {
+                command.RaiseCanExecuteChanged();
+            }
+        }
+    }
+}
